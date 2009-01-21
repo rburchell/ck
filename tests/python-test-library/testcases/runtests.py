432e5aee9a5599644fa82ac65ae72383ceb4bd49
lamadon <jean-luc.lamadon@nokia.com>
2009-01-21 09:15:50 +0200

    Stripped down runtests.py for continuous integration

--- tests/python-test-library/testcases/runtests.py
+++ tests/python-test-library/testcases/runtests.py
@@ -13,8 +13,8 @@
 ##
 ## 
 import sys
-sys.path.append("../testcases")
-sys.path.append("../stubs")
+sys.path.append("tests/python-test-library/testcases")
+sys.path.append("tests/python-test-library/stubs")
 import os
 from time import sleep
 import dbus
@@ -22,7 +22,7 @@ import commands
 import dbus_emulator
 import conf
 import contextGetPropertiesTCs
-#import contextSubscribeTCs
+#import contextSubscribePropertiesTCs
 import test
 
 
@@ -36,7 +36,7 @@ def module_setUp():
     sleep (5)
     print "Starting contextd"
     os.system(conf.contextSrcPath + os.path.sep + "contextd &")
-    #sleep (5)
+    sleep (5)
     #os.system(conf.contextSrcPath + os.sep + "tests/python-test-library/testcases/test.py &")
     #sleep (5)
     
@@ -47,180 +47,12 @@ def module_tearDown():
     dbus_emulator.deleteBus(dbus_emulator.SESSION)
 
 
-def get_mce_props():
-    # Get the corresponding property from contextd
-    edge_key = "Context.Device.Orientation.edgeUp"
-    facing_key = "Context.Device.Orientation.facingUp"
-    display_key = "Context.Device.Display.displayState"
-    flight_key = "Context.Environment.Connection.CurrentData.isFlightMode"
-    emergency_key = "Context.Environment.Connection.CurrentData.isEmergencyMode"
-    activity_key = "Context.Device.Usage.inActiveUse";
-    
-    props = []
-    
-    sesBus = dbus.SessionBus()
-    try:
-        object  = sesBus.get_object("org.freedesktop.ContextKit","/org/freedesktop/ContextKit/Manager")
-        iface = dbus.Interface(object,"org.freedesktop.ContextKit.Manager")
-        ret = iface.Get([edge_key, facing_key, display_key, flight_key, emergency_key, activity_key])        
-            
-        props = [ret[edge_key][1], ret[facing_key][1], ret[display_key][1], ret[flight_key][1], ret[emergency_key][1], ret[activity_key][1]]
-        
-    except dbus.DBusException:
-        print "Caught an exception"
-        traceback.print_exc()
-    
-    return props
-
-def check_props(props, correct):
-    print props
-    
-    if len(props) != len(correct):
-        print "Wrong length of properties", props
-        return False
-    
-    for i in range(len(props)):
-        if props[i] != correct[i]:
-            print "Property failed, they are:", props
-            return False
-    return True
-
-def execute_get_tests(ifceMCE):
-    # Facing down
-    ifceMCE.req_device_facing_change("face_down")
-
-    props = get_mce_props()
-    if check_props(props, [0, 2, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Facing up
-    ifceMCE.req_device_facing_change("face_up")   
-    
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    	
-    # FIXME: Add orientation tests
-   
-    # Dim display
-    ifceMCE.req_display_state_dim()
-    
-    props = get_mce_props()
-    if check_props(props, [0, 1, 2, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Display off
-    ifceMCE.req_display_state_off()
-    
-    props = get_mce_props()
-    if check_props(props, [0, 1, 0, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Display on again
-    ifceMCE.req_display_state_on()
-    
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-
-    # Flight mode
-    ifceMCE.req_device_mode_change("flight")
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, True, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    	
-    # Normal mode again
-    ifceMCE.req_device_mode_change("normal")
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Normal call
-    ifceMCE.req_call_state_change("cellular", "normal")
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Normal call ends
-    ifceMCE.req_call_state_change("none", "normal")
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    	
-    # Emergency call
-    ifceMCE.req_call_state_change("cellular", "emergency")
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, True, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Emergency call ends
-    ifceMCE.req_call_state_change("none", "emergency")
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    	
-    # Device inactive
-    ifceMCE.req_inactivity_status_inactive()
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, False]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-    
-    # Device active again
-    ifceMCE.req_inactivity_status_active()
-    props = get_mce_props()
-    if check_props(props, [0, 1, 1, False, False, True]) == True:
-        print "## SUCCESS ##"
-    else:
-    	print "## FAIL ##"
-
-def execute_subscribe_tests(ifceMCE):
-    # FIXME: We need the unsubscription to work to test this, right?
-    
-
 # Main stuff
 if __name__ == "__main__":
     print "runtests.py main"
     print "Starting tests"
     module_setUp()
-    #contextGetPropertiesTCs.testRun()
-    #test.testRun()
-    #module_tearDown()
-    
-    sysBus = dbus.SystemBus()
-    try:
-        object  = sysBus.get_object("com.nokia.mce","/com/nokia/mce/request")
-        ifceMCE = dbus.Interface(object,"com.nokia.mce.request")
-    except dbus.DBusException:
-        traceback.print_exc()
-        sys.exit(1)
-
-    execute_get_tests(ifceMCE)
-    
-    execute_subscribe_tests(ifceMCE)
-    
+    contextGetPropertiesTCs.testRun()
+    #contextSubscribePropertiesTCs.testRun()
     print "Tearing down tests"
     module_tearDown()
