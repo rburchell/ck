efdf861d9b699b59e8b7e0ad2ea6e185155f8727
Rob Taylor <rob.taylor@codethink.co.uk>
2009-02-03 22:07:40 +0000

    ctypes binding now covers all the of context_provider base api

--- python/ContextProvider.py
+++ python/ContextProvider.py
@@ -1,5 +1,3 @@
-import gobject
-
 from ctypes import *
 from CTypesHelpers import *
 
@@ -34,10 +32,44 @@ class ContextProvider:
         self.no_of_subscribers = cfunc('context_provider_no_of_subscribers', self.dll, c_int,
                                     ('key', c_char_p, 1))
 
-        self.change_set_create = ('context_provider_change_set_create', self.dll, CHANGE_SET,
-                                    None);
+        self.change_set_create = cfunc('context_provider_change_set_create', self.dll, CHANGE_SET)
+        self.change_set_commit = cfunc('context_provider_change_set_commit', self.dll, None,
+                                    ('change_set', CHANGE_SET, 1))
+        self.change_set_cancel = cfunc('context_provider_change_set_cancel', self.dll, None,
+                                    ('change_set', CHANGE_SET, 1))
 
+        self.change_set_add_int = cfunc('context_provider_change_set_add_int', self.dll, c_int,
+                                    ('change_set', CHANGE_SET, 1),
+                                    ('key', c_char_p, 1),
+                                    ('val', c_int, 1))
+        self.change_set_add_double = cfunc('context_provider_change_set_add_double', self.dll, c_int,
+                                    ('change_set', CHANGE_SET, 1),
+                                    ('key', c_char_p, 1),
+                                    ('val', c_double, 1))
+
+        self.change_set_add_undetermined_key = cfunc('context_provider_change_set_add_undetermined_key',
+                                    self.dll, c_int,
+                                    ('change_set', CHANGE_SET, 1),
+                                    ('key', c_char_p, 1))
 
 if __name__ == "__main__":
+    import gobject
+    import dbus
+    import dbus.service
+    from dbus.mainloop.glib import DBusGMainLoop
+    DBusGMainLoop(set_as_default=True)
+    loop = gobject.MainLoop()
+
+    busname = dbus.service.BusName("org.freedesktop.ContextKit.Testing.Provider",
+                                    dbus.SessionBus())
+
     cp = ContextProvider()
     cp.init(["foo.bar", "foo.baz"], 1, None, None, None, None, None, None)
+    print cp.no_of_subscribers("foo.bar")
+    cs = cp.change_set_create()
+    cp.change_set_cancel(cs)
+    cs2 = cp.change_set_create()
+    cp.change_set_add_int(cs2, "foo.bar", 1)
+    cp.change_set_add_undetermined_key(cs2, "foo.baz")
+    cp.change_set_commit(cs2)
+    loop.run()
