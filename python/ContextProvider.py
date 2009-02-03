d1b4ef3452abe1d59e2bf3247238f285faf13c64
Rob Taylor <rob.taylor@codethink.co.uk>
2009-02-03 22:11:15 +0000

    Change everything to static members for ctypes api binding

--- python/ContextProvider.py
+++ python/ContextProvider.py
@@ -9,48 +9,40 @@ class CHANGE_SET(c_void_p):
     pass
 
 class ContextProvider:
-    def __init__(self):
-        self.define_types()
-        self.define_functions()
+    _dll = CDLL("libcontextprovider.so")
+    STRING_ARRAY = POINTER(c_char_p)
+    GET_CALLBACK = POINTER(CFUNCTYPE(STRING_SET, CHANGE_SET, c_void_p))
+    SUBSCRIBE_CALLBACK = POINTER(CFUNCTYPE(STRING_SET, c_void_p))
 
-    def define_types(self):
-        self.dll = CDLL("libcontextprovider.so")
-        self.STRING_ARRAY = POINTER(c_char_p)
-        self.GET_CALLBACK = POINTER(CFUNCTYPE(STRING_SET, CHANGE_SET, c_void_p))
-        self.SUBSCRIBE_CALLBACK = POINTER(CFUNCTYPE(STRING_SET, c_void_p))
+    init = cfunc('context_provider_init', _dll, None,
+                 ('provided_keys', ListPOINTER (c_char_p), 1),
+                 ('useSessionBus', c_int, 1),
+                 ('get_cb', GET_CALLBACK, 1),
+                 ('get_cb_target', c_void_p, 1),
+                 ('first_cb', SUBSCRIBE_CALLBACK, 1),
+                 ('first_cb_target', c_void_p, 1),
+                 ('last_cb', SUBSCRIBE_CALLBACK, 1),
+                 ('last_cb_target', c_void_p, 1))
+    no_of_subscribers = cfunc('context_provider_no_of_subscribers', _dll, c_int,
+                              ('key', c_char_p, 1))
 
-    def define_functions(self):
-        self.init = cfunc('context_provider_init', self.dll, None,
-                            ('provided_keys', ListPOINTER (c_char_p), 1),
-                            ('useSessionBus', c_int, 1),
-                            ('get_cb', self.GET_CALLBACK, 1),
-                            ('get_cb_target', c_void_p, 1),
-                            ('first_cb', self.SUBSCRIBE_CALLBACK, 1),
-                            ('first_cb_target', c_void_p, 1),
-                            ('last_cb', self.SUBSCRIBE_CALLBACK, 1),
-                            ('last_cb_target', c_void_p, 1))
-        self.no_of_subscribers = cfunc('context_provider_no_of_subscribers', self.dll, c_int,
-                                    ('key', c_char_p, 1))
+    change_set_create = cfunc('context_provider_change_set_create', _dll, CHANGE_SET)
+    change_set_commit = cfunc('context_provider_change_set_commit', _dll, None,
+                              ('change_set', CHANGE_SET, 1))
+    change_set_cancel = cfunc('context_provider_change_set_cancel', _dll, None,
+                              ('change_set', CHANGE_SET, 1))
+    change_set_add_int = cfunc('context_provider_change_set_add_int', _dll, c_int,
+                               ('change_set', CHANGE_SET, 1),
+                               ('key', c_char_p, 1),
+                               ('val', c_int, 1))
+    change_set_add_double = cfunc('context_provider_change_set_add_double', _dll, c_int,
+                                  ('change_set', CHANGE_SET, 1),
+                                  ('key', c_char_p, 1),
+                                  ('val', c_double, 1))
 
-        self.change_set_create = cfunc('context_provider_change_set_create', self.dll, CHANGE_SET)
-        self.change_set_commit = cfunc('context_provider_change_set_commit', self.dll, None,
-                                    ('change_set', CHANGE_SET, 1))
-        self.change_set_cancel = cfunc('context_provider_change_set_cancel', self.dll, None,
-                                    ('change_set', CHANGE_SET, 1))
-
-        self.change_set_add_int = cfunc('context_provider_change_set_add_int', self.dll, c_int,
-                                    ('change_set', CHANGE_SET, 1),
-                                    ('key', c_char_p, 1),
-                                    ('val', c_int, 1))
-        self.change_set_add_double = cfunc('context_provider_change_set_add_double', self.dll, c_int,
-                                    ('change_set', CHANGE_SET, 1),
-                                    ('key', c_char_p, 1),
-                                    ('val', c_double, 1))
-
-        self.change_set_add_undetermined_key = cfunc('context_provider_change_set_add_undetermined_key',
-                                    self.dll, c_int,
-                                    ('change_set', CHANGE_SET, 1),
-                                    ('key', c_char_p, 1))
+    change_set_add_undetermined_key = cfunc('context_provider_change_set_add_undetermined_key', _dll, c_int,
+                                            ('change_set', CHANGE_SET, 1),
+                                            ('key', c_char_p, 1))
 
 if __name__ == "__main__":
     import gobject
@@ -63,13 +55,12 @@ if __name__ == "__main__":
     busname = dbus.service.BusName("org.freedesktop.ContextKit.Testing.Provider",
                                     dbus.SessionBus())
 
-    cp = ContextProvider()
-    cp.init(["foo.bar", "foo.baz"], 1, None, None, None, None, None, None)
-    print cp.no_of_subscribers("foo.bar")
-    cs = cp.change_set_create()
-    cp.change_set_cancel(cs)
-    cs2 = cp.change_set_create()
-    cp.change_set_add_int(cs2, "foo.bar", 1)
-    cp.change_set_add_undetermined_key(cs2, "foo.baz")
-    cp.change_set_commit(cs2)
+    ContextProvider.init(["foo.bar", "foo.baz"], 1, None, None, None, None, None, None)
+    print ContextProvider.no_of_subscribers("foo.bar")
+    cs = ContextProvider.change_set_create()
+    ContextProvider.change_set_cancel(cs)
+    cs2 = ContextProvider.change_set_create()
+    ContextProvider.change_set_add_int(cs2, "foo.bar", 1)
+    ContextProvider.change_set_add_undetermined_key(cs2, "foo.baz")
+    ContextProvider.change_set_commit(cs2)
     loop.run()
