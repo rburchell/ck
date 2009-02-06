1a5f07e5dd647b6680dc2b4f0f363762253e77f4
Jean-Luc Lamadon <jean-luc.lamadon@nokia.com>
2009-02-06 09:25:47 +0200

    Added more properties

--- tests/python-test-library/testcases/conf.py
+++ tests/python-test-library/testcases/conf.py
@@ -19,6 +19,18 @@ ctxBusName = "org.freedesktop.ContextKit"
 ctxMgrPath = "/org/freedesktop/ContextKit/Manager"
 ctxMgrIfce = "org.freedesktop.ContextKit.Manager"
 
+ctxScriberIfce = "org.freedesktop.ContextKit.Subscriber"
+
+mceBusName = "com.nokia.mce"
+mceRqstPath = "/com/nokia/mce/request"
+mceRqstIfce = "com.nokia.mce.request"
+
+testBusName = "org.freedesktop.context.testing"
+testRqstPath = "/org/freedesktop/context/testing/request"
+testRqstIfce = "org.freedesktop.context.testing.request"
+
 scriberOnePath = "/org/freedesktop/ContextKit/Subscribers/0"
 scriberTwoPath = "/org/freedesktop/ContextKit/Subscribers/1"
 scriberThirdPath = "/org/freedesktop/ContextKit/Subscribers/2"
+
+properties = ['Context.Device.Orientation.facingUp','Context.Device.Orientation.edgeUp']
