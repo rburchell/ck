6c2f6698bd6079979e5fecbd5aab26cd04094233
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2009-05-12 09:23:26 +0300

    Adding licenses, removing unnecessary forward declaration.

--- libcontextsubscriber/src/managerinterface.h
+++ libcontextsubscriber/src/managerinterface.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2008, 2009 Nokia Corporation.
+ *
+ * Contact: Marius Vollmer <marius.vollmer@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
 #ifndef MANAGERINTERFACE_H
 #define MANAGERINTERFACE_H
 
@@ -8,9 +29,6 @@
 /*
  * Proxy class for interface org.freedesktop.ContextKit.Manager
  */
-
-class ManagerSignallingInterface;
-
 
 class ManagerInterface : public QObject
 {
