3a1f2a760095c671f546e8af812627a4acfd9322
Gergely Risko <gergely+context@risko.hu>
2009-06-22 08:36:31 +0300

    POSIXified rec-kill.sh.
    Signed-off-by: Jean-Luc Lamadon <jean-luc.lamadon@nokia.com>

--- libcontextsubscriber/customer-tests/common/rec-kill.sh
+++ libcontextsubscriber/customer-tests/common/rec-kill.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 dokill() {
-    for cpid in $(ps -o pid= --ppid $1)
+    for cpid in `ps -o pid= --ppid $1`
     do
         dokill $cpid
     done
-    #echo "killing: $(ps -p $1 -o cmd=)"
     kill -9 $1 > /dev/null 2>&1
 }
 
-if [[ $# == 0 ]]; then
-    echo "usage: $(basename $0) <top pid to kill>"
+if test $# = 0
+then
+    echo "usage: $0 <top pid to kill>"
     exit 1
 fi
 
