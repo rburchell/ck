60e7c803faea418294c1b0216c090fac783537de
David Janzso <ext-david.2.janzso@nokia.com>
2009-06-26 17:32:42 +0300

    Automatic registry changes every ~2 seconds.
    - set POSIX time as the value for test.int and test.int2
    - add gitignore

--- libcontextsubscriber/multithreading-tests/stress-test/runme.sh
+++ libcontextsubscriber/multithreading-tests/stress-test/runme.sh
@@ -1,2 +1,4 @@
-xterm -e 'rlwrap ../../../python/context-provide  my.test.provider int test.int 1' &
-CONTEXT_PROVIDERS=. ./stress-test 5
+#xterm -e 'rlwrap ../../../python/context-provide  my.test.provider int test.int 0 int test.int2 0' &
+#xterm -e 'rlwrap ../../../python/context-provide  my.test2.provider int test2.int 0 int test2.int2 0' &
+xterm -e './provider.py' &
+CONTEXT_PROVIDERS=../stress-test/ ./stress-test 3 3
