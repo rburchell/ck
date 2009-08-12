ab1dccdaa17914259ae2e86961fecd00ddef0877
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2009-08-12 12:42:45 +0300

    libcontextprovider, cust.tests: Test program can now both write to client and read from client. Added first real test.

--- libcontextprovider/customer-tests/c-api/cprovider.c
+++ libcontextprovider/customer-tests/c-api/cprovider.c
@@ -26,6 +26,11 @@
 #include <dbus/dbus.h>
 #include <glib-object.h>
 #include <assert.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <string.h>
 
 /* Keys (names of the context properties) divided in groups.
 */
@@ -91,6 +96,47 @@ void single2_cb(int subscribed, void* user_data)
     single2_subscribed = subscribed;
 }
 
+/* Pipes to and from the client program.  The test program commands
+   the client and checks the output through them.
+ */
+int tube[2];
+char line_from_client[1024];
+int chars_read = 0;
+
+char* write_to_client(char* command)
+{
+    fd_set read_fds;
+    struct timeval tv;
+    int ret = -1;
+
+    int temp;
+    /* Write the command to the client */
+    temp = dprintf(tube[1], command);
+
+    /* Process the events until we can read from the client */
+    do {
+        /*fprintf(stderr, "processing\n");*/
+        g_main_context_iteration(NULL, TRUE);
+
+        FD_ZERO(&read_fds);
+        FD_SET(tube[1], &read_fds);
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+
+        /* Note: the "number of file descriptors to check" is not
+           1. It means "starting from file descriptor 0, how many file
+           descriptors I need to check".
+        */
+        ret = select(tube[1]+1, &read_fds, NULL, NULL, &tv);
+    } while (ret == 0 || ret == -1);
+
+    chars_read = read(tube[1], line_from_client, 1023);
+    line_from_client[chars_read] = '\0';
+    /*fprintf(stderr, "%s\n", line_from_client);*/
+
+    return 0;
+}
+
 /*
   Test cases. Note that the test cases are *not* independent and
   include only a minimal set of tests for the C api.
@@ -100,7 +146,7 @@ int test_init()
 {
     /* Test: initialize using the library */
     int ret = context_provider_init (DBUS_BUS_SESSION,
-                                     "com.nokia.test");
+                                     "org.freedesktop.ContextKit.testProvider");
     /* Expected result: return value == success */
     if (!ret) return 1;
 
@@ -120,6 +166,15 @@ int test_init()
 
 int test_get_subscriber()
 {
+    /* Test: Command the client to execute GetSubscriber over DBus. */
+    write_to_client("getsubscriber session org.freedesktop.ContextKit.testProvider\n");
+
+    /* Expected result: the client got a correct subscriber path */
+    int mismatch = strncmp(line_from_client,
+                           "GetSubscriber returned "
+                           "/org/freedesktop/ContextKit/Subscriber/0",
+                           chars_read-1);
+    if (mismatch) return 1;
     return 0;
 }
 
@@ -155,28 +210,55 @@ int run_tests()
     }
     fprintf(stderr, "SUCCESS\n");
 
+    fprintf(stderr, "Running test_get_subscriber... ");
+    ret = test_get_subscriber();
+    if (ret) {
+        fprintf(stderr, "FAIL\n");
+        return ret;
+    }
+    fprintf(stderr, "SUCCESS\n");
+
     /* All tests successful */
     return 0;
 }
 
 int main(int argc, char **argv)
 {
-    /* Initialization */
     GMainLoop *loop;
+    int ret;
+
+    /* Initialization */
     g_type_init ();
     loop = g_main_loop_new (NULL, FALSE);
 
     /* Start the client program */
-    FILE *client;
-    client = popen("client 2>/dev/null", "w");
-    assert(client);
+    ret = socketpair(AF_UNIX, SOCK_STREAM, 0, tube);
+    assert(ret == 0);
 
-    /* Test cases */
-    int ret = run_tests();
+    if (fork() == 0) {
+        /* child process */
 
-    /* Command the client to exit */
-    pclose(client);
+        close(tube[1]);
+        dup2(tube[0], 0);   /* standard input from the stream pipe */
+        dup2(tube[0], 1);   /* standard output to the same descriptor */
 
-    /* Return the success / failure value */
-    return ret;
+        char* arg_list[] = {"client", NULL};
+        execvp("client", arg_list);
+
+        return 1;
+    } else {
+        /* parent process */
+        close(tube[0]);
+        sleep(1);
+
+        /* Test cases */
+        int ret = run_tests();
+
+        /* Command the client to exit */
+        close(tube[1]);
+
+        /* Return the success / failure value */
+        return ret;
+    }
+    return 0;
 }
