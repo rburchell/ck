e53c396991c37b7686a4a5527af18bd500ba5575
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2009-09-04 10:43:09 +0300

    libcontextsubscriber, unit tests: Automatical creation of data bases for dynamic tests.

--- libcontextsubscriber/unit-tests/cdb-creation/copy.sh
+++ libcontextsubscriber/unit-tests/cdb-creation/copy.sh
@@ -2,3 +2,13 @@
 
 cp simple/cache.cdb ../contextpropertyinfo-cdb-static/cache.cdb
 cp simple/cache.cdb ../contextregistryinfo-cdb-static/cache.cdb
+
+cp dynamic/providers2v1/cache.cdb ../contextpropertyinfo-cdb-dynamic/context-providers2v1.cdb
+cp dynamic/providers2v2/cache.cdb ../contextpropertyinfo-cdb-dynamic/context-providers2v2.cdb
+
+cp dynamic/providers2v1/cache.cdb ../contextpropertyinfo-cdb-dynamic/context-providers3v1.cdb
+cp dynamic/providers3v2/cache.cdb ../contextpropertyinfo-cdb-dynamic/context-providers3v2.cdb
+
+cp dynamic/providers2v1/cache.cdb ../contextpropertyinfo-cdb-dynamic/context-providers4v1.cdb
+cp dynamic/providers4v2/cache.cdb ../contextpropertyinfo-cdb-dynamic/context-providers4v2.cdb
+
--- libcontextsubscriber/unit-tests/cdb-creation/create.sh
+++ libcontextsubscriber/unit-tests/cdb-creation/create.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -e
 
 ../../update-contextkit-providers/update-contextkit-providers simple
+
+../../update-contextkit-providers/update-contextkit-providers dynamic/providers2v1
+../../update-contextkit-providers/update-contextkit-providers dynamic/providers2v2
+../../update-contextkit-providers/update-contextkit-providers dynamic/providers3v2
+../../update-contextkit-providers/update-contextkit-providers dynamic/providers4v2
+
