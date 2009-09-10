5bbb1ded3cbdae493fd9e7f3f39bfb01ba360836
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2009-09-10 16:26:17 +0300

    bluez plugin: Fixing the customer tests after plugin-loading modifications.

--- libcontextsubscriber/customer-tests/runTests.sh
+++ libcontextsubscriber/customer-tests/runTests.sh
@@ -5,7 +5,7 @@ DIRS="commander subscription asynchronicity registry bluez-plugin2"
 if pkg-config contextprovider-1.0 || [ -e ../../libcontextprovider/src/.libs/libcontextprovider.so ]
 then
 	export CONTEXT_PROVIDERS=.
-	export LD_LIBRARY_PATH=../../src/.libs:../../../libcontextprovider/src/.libs
+	export LD_LIBRARY_PATH=../../src/.libs:../../plugins/bluez/.libs:../../../libcontextprovider/src/.libs
 	export PATH=$PATH:../../../python:../../cli:../../reg-cli
 
 	for dir in $DIRS; do
