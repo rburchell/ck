b25ecd59435b7a2037ae1b712a3aabb3930cd9b5
Michael Dominic K <mdk@codethink.co.uk>
2009-09-24 11:37:55 +0200

    Introducing the context-provide dir & build system.

---
+++ libcontextprovider/context-provide/context-provide.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <QString>
+#include <QStringList>
+#include <QMap>
+#include <QDebug>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    return app.exec();
+}
