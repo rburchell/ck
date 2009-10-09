5f270f20bdc8c7fea6965962636cd4dce007975a
Gergely Risko <gergely+context@risko.hu>
2009-10-09 14:12:31 +0300

    libcontext{subscriber,provider}: coverage support for customer-tests

--- libcontextsubscriber/customer-tests/runTests.sh
+++ libcontextsubscriber/customer-tests/runTests.sh
@@ -4,6 +4,16 @@ cd $(dirname $0)
 DIRS="commander subscription asynchronicity registry pluginchanging"
 . ./env.sh
 
+if [ -n "$COVERAGE" ]
+then
+    make -C coverage-build
+    export LD_LIBRARY_PATH=../coverage-build/.libs:$LD_LIBRARY_PATH
+    rm -rf coverage-build/.libs/*.gcda
+else
+    make -C ../src
+fi
+
+make -C testplugins
 
 if pkg-config contextprovider-1.0 || [ -e ../../libcontextprovider/src/.libs/libcontextprovider.so ]
 then
@@ -18,4 +28,17 @@ then
 else
 	echo "libcontextprovider is not installed nor built"
 	exit 1
+fi
+
+echo "Running update-contextkit-providers customer check"
+( cd update-contextkit-providers && ./test.sh )
+
+if [ -n "$COVERAGE" ]
+then
+    echo "computing coverage"
+    rm -rf coverage
+    mkdir -p coverage
+    lcov --directory coverage-build/.libs/ --capture --output-file coverage/all.cov
+    lcov --extract coverage/all.cov '*/src/*.cpp' --output-file coverage/src.cov
+    genhtml -o coverage/ coverage/src.cov
 fi
