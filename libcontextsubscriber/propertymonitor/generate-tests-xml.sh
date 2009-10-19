bc48f9aa031fa4960fd4ae4b6af46abcc6985fce
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2009-10-19 17:29:56 +0300

    Property monitor: Fixing bugs related to vpath builds.

--- libcontextsubscriber/propertymonitor/generate-tests-xml.sh
+++ libcontextsubscriber/propertymonitor/generate-tests-xml.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -e
 
-cat tests.xml.begin > tests.xml
+if [ $# -ne 4 ]
+then
+    echo "Usage: generate-tests-xml.sh COREPATH BEGIN_TEMPLATE MIDDLE_TEMPLATE END_TEMPLATE"
+    exit 1
+fi
 
-export CONTEXT_CORE_DECLARATIONS=../../spec/core.context
+# Beginning of the tests.xml template
+cat $2 > tests.xml
+
+export CONTEXT_CORE_DECLARATIONS=$1
 # core properties start with a capital letter; list them
 for prop in `../cls/context-ls | grep ^[A-Z] | sort`
 do
     # for each core property, create a test case
-    sed "s/PROPNAME/$prop/g" tests.xml.middle >> tests.xml
+    sed "s/PROPNAME/$prop/g" $3 >> tests.xml
 done
 
-cat tests.xml.end >> tests.xml
+# End of the tests.xml template
+cat $4 >> tests.xml
