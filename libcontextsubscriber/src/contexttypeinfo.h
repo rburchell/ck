f192e51d41812a2589a9f84553e429fb0a078651
Michael Dominic K <mdk@codethink.co.uk>
2009-11-04 16:34:59 +0100

    Base of new ContextTypeInfo.

---
+++ libcontextsubscriber/src/contexttypeinfo.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008 Nokia Corporation.
+ *
+ * Contact: Marius Vollmer <marius.vollmer@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef CONTEXTTYPEINFO_H
+#define CONTEXTTYPEINFO_H
+
+#include <QVariant>
+#include <QStringList>
+#include <QObject>
+#include "nanotree.h"
+
+class ContextTypeInfo : public QObject
+{
+    Q_OBJECT
+
+public:
+    ContextTypeInfo(QString name);
+    QString name();
+
+private:
+    QString typeName;
+};
+
+#endif
