eef80f0aec644867ab1659ba70ba241dc91b1734
Akos PASZTORY <ext-akos.pasztory@nokia.com>
2009-11-12 12:58:56 +0200

    backward compatibility quick-fix for customer-tests
    Added enough mapping to old types to make the customer tests pass.

--- libcontextprovider/customer-tests/multiprovider/env.sh
+++ libcontextprovider/customer-tests/multiprovider/env.sh
@@ -2,5 +2,5 @@
 
 export PYTHONPATH=$PYTHONPATH:../../../python
 export CONTEXT_PROVIDERS=.
-export LD_LIBRARY_PATH=../../src/.libs:../../../libcontextsubscriber/src/.libs
+export LD_LIBRARY_PATH=../../src/.libs:../../../libcontextsubscriber/src/.libs:$LD_LIBRARY_PATH
 export PATH=../../../python:../../context-provide:../../../libcontextsubscriber/cli:../../../libcontextsubscriber/cls:../client:$PATH
