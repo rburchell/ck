rburchell/ck

commit 8fe4477f8149db26cedb16155e02185d7c7ae2f1

lcs: Simplifying the contract between contextkit and plugins.
Emitting ready() when the plugin is already in ready state has no effect.
Previously this caused the upper layer to resubscribe. Now, to cause that
effect, emit failed() and then ready(). No plugins rely on the old behaviour,
afaik.

libcontextsubscriber/src/contextkitplugin.cpp
@@ -89,9 +89,7 @@ ContextKitPlugin::ContextKitPlugin(const QDBusConnection bus, const QString& bus
       connection(new QDBusConnection(bus)),
       busName(busName),
       newProtocol(true),
-      defaultNewProtocol(true),
-      providerAppearedQueued(false),
-      providerAppearedSkip(false)
+      defaultNewProtocol(true)
 {
     reset();
     // Notice if the provider on the dbus comes and goes
@@ -105,7 +103,6 @@ ContextKitPlugin::ContextKitPlugin(const QDBusConnection bus, const QString& bus
     // the provider at startup, whether it's present or not.
     providerListener->startListening(false);
     QMetaObject::invokeMethod(this, "onProviderAppeared", Qt::QueuedConnection);
-    providerAppearedQueued = true;
 }
 
 void ContextKitPlugin::setDefaultNewProtocol(bool s) {
@@ -128,24 +125,6 @@ void ContextKitPlugin::reset()
 /// appears.
 void ContextKitPlugin::onProviderAppeared()
 {
-    // It is possible that this function is called and we have a Subscribe call
-    // in progress.  This happens when things happen in the following order:
-    // 1. the subscriber is started
-    // 2. the subscriber optimistically sends the Subscribe call
-    // 3. the provider is started (quick enough to handle the Subscribe call)
-    // 4. providerListener notices that the provider was started
-    // In this case, the plugin is in "ready" state already.
-
-    providerAppearedQueued = false;
-
-    if (providerAppearedSkip) {
-        // Now we had queued this function to be called, but before it was
-        // called, waitUntilReadyAndBlock called it.  Skip the scheduled call
-        // (but not the later calls).
-        providerAppearedSkip = false;
-        return;
-    }
-
     contextDebug() << "Provider appeared:" << busName;
 
     reset();
@@ -402,14 +381,10 @@ void ContextKitPlugin::onNewValueChanged(QList<QVariant> value,
 
 void ContextKitPlugin::blockUntilReady()
 {
-    bool setSkip = providerAppearedQueued; // save this value
+    // This will result in emitting ready() immediately; we don't really block.
+    // This optimistic plugin is in "ready" state even if it's not sure whether
+    // the provider is running.
     onProviderAppeared();
-    if (setSkip) {
-        // Calling onProviderAppeared was already scheduled but we already
-        // called it now.  Arrange so that the scheduled call is skipped when
-        // the event is processed.
-        providerAppearedSkip = true;
-    }
 }
 
 void ContextKitPlugin::blockUntilSubscribed(const QString& key)

libcontextsubscriber/src/contextkitplugin.h
@@ -119,9 +119,6 @@ private:
 
     QHash<QString, PendingSubscribeWatcher*> pendingWatchers;
     QSet<QString> pendingKeys;
-
-    bool providerAppearedQueued;
-    bool providerAppearedSkip;
 };
 
 QVariant demarshallValue(const QVariant &v);

libcontextsubscriber/src/provider.cpp
@@ -215,6 +215,10 @@ void Provider::onPluginReady()
 {
     contextDebug();
 
+    // Ignore the signal if the plugin is already in the ready state.
+    if (pluginState == READY)
+        return;
+
     QMutexLocker lock(&subscribeLock);
     // Renew the subscriptions (if any).
     // Renewing happens when a provider has disappeared and now it appeared again.

commit ffba1e64b58b408689e8238c61d757789ad90d6d

lcs: fixing regression caused by 8fe4477f8149db26cedb16155e02185d7c7ae2f1.
This caused subscribers started before providers never receive the context
property values.

Now emitting ready() 2 times has no effect. The plugin must emit failed() in
between for the second ready() to have an effect.

Also includes an ugly hack for distinguishing "plugin failed because the
subscriber was started first" from interesting failures.

This also requires corresponding changes in contextkit-maemo.

libcontextsubscriber/src/contextkitplugin.cpp
@@ -147,7 +147,7 @@ void ContextKitPlugin::onProviderDisappeared()
 {
     contextDebug() << "Provider disappeared:" << busName;
     reset();
-    Q_EMIT failed("Provider went away " + busName);
+    Q_EMIT failed("Provider not present: " + busName);
 }
 
 /// Starts using the fresh subscriber interface when it is returned by
@@ -282,6 +282,11 @@ void ContextKitPlugin::newSubscribe(const QString& key)
              SIGNAL(subscribeFailed(QString,QString)),
              this,
              SLOT(removePendingWatcher(const QString&)));
+    sconnect(psw,
+             SIGNAL(providerNotPresent()),
+             this,
+             SLOT(onProviderDisappeared()));
+
 }
 
 
@@ -422,6 +427,11 @@ void PendingSubscribeWatcher::onFinished()
     QDBusPendingReply<QList<QVariant>, quint64> reply = *this;
     if (reply.isError()) {
         Q_EMIT subscribeFailed(key, reply.error().message());
+        if (reply.error().type() == QDBusError::ServiceUnknown) {
+            // We need to distinguish this case, so that the plugin can emit
+            // failed(), and become ready again when the provider appears.
+            Q_EMIT providerNotPresent();
+        }
         return;
     }
 

libcontextsubscriber/src/contextkitplugin.h
@@ -57,6 +57,7 @@ Q_SIGNALS:
     void subscribeFailed(QString, QString);
     void valueChanged(QString, TimedValue);
     void subscribeFinished(QString);
+    void providerNotPresent();
 
 private:
     QString key;

libcontextsubscriber/src/provider.cpp
@@ -241,7 +241,16 @@ void Provider::clearValues()
 /// for keys we are trying to subscribe to.
 void Provider::onPluginFailed(QString error)
 {
-    contextWarning() << error;
+    // This function is called during "normal operation" when the subscriber is
+    // started before the provider. Contextkit recovers from that. Hence, don't
+    // print out a warning in that case.
+    if (error.startsWith("Provider not present"))
+        contextDebug() << error;
+    else
+        // but do print out the interesting warnings
+        contextWarning() << error;
+    // TODO: startsWith is ugly, but we don't have error numbers to make this
+    // more elegant.
 
     QMutexLocker lock(&subscribeLock);
     pluginState = FAILED;